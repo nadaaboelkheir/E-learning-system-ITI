68155b852d5bd761e9bd5feb0fa007131f6cc8f3
nada <nadaaboelkheir1@gmail.com>
2024-10-12 02:17:26 +0300

    fix : jwt verify

--- src/middlewares/auth.mw.js
+++ src/middlewares/auth.mw.js
@@ -1,53 +1,46 @@
 const jwt = require('jsonwebtoken');
 const { Teacher } = require('../models');
 const { JWT_SECRET } = require('../utils/env');
+
 exports.protectRoute = async (req, res, next) => {
 	const token =
 		req.cookies['access-token'] ||
-		(req.headers.authorization
-			? req.headers.authorization.split(' ')[1]
-			: null);
+		(req.headers.authorization ? req.headers.authorization.split(' ')[1] : null);
+
 	if (!token) {
 		return res.status(401).json({ error: 'لا تستطيع الوصول لهذه الصفحة' });
 	}
-	try {
-		const decoded = jwt.verify(
-			token,
-			process.env.JWT_SECRET,
-			(err, decoded) => {
-				if (err) {
-					if (err.message === 'jwt expired') {
-						return res
-							.status(401)
-							.json({ error: 'تم انتهاء الصلاحية' });
-					}
-					return res.status(401).json({ error: err.message });
-				}
-				return decoded;
-			},
-		);
+
+	jwt.verify(token, JWT_SECRET, async (err, decoded) => {
+		if (err) {
+			if (err.message === 'jwt expired') {
+				return res.status(401).json({ error: 'تم انتهاء الصلاحية' });
+			}
+			return res.status(401).json({ error: err.message });
+		}
 
 		req.userId = decoded.id;
 		req.role = decoded.role;
 
-		// check token invalid or not
 		if (decoded.role === 'admin') {
 			req.admin = { id: decoded.id, role: decoded.role };
 		} else if (decoded.role === 'student') {
 			req.student = { id: decoded.id, role: decoded.role };
 		} else if (decoded.role === 'teacher') {
-			const teacher = await Teacher.findOne({
-				where: { id: decoded.id },
-				attributes: ['id', 'role', 'isEmailVerified'],
-			});
-			if (!teacher) {
-				return res.status(401).json({ error: 'Teacher not found' });
+			try {
+				const teacher = await Teacher.findOne({
+					where: { id: decoded.id },
+					attributes: ['id', 'role', 'isEmailVerified'],
+				});
+				if (!teacher) {
+					return res.status(401).json({ error: 'Teacher not found' });
+				}
+				req.teacher = teacher;
+			} catch (error) {
+				return res.status(500).json({ error: 'Internal server error' });
 			}
-			req.teacher = teacher;
 		}
 
 		next();
-	} catch (error) {
-		return res.status(401).json({ error: 'Internal server error' });
-	}
+	});
 };
